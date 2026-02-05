8697f91c9cc7feab87e097d15e016d63e6c37516
ebadarshad <m.ebadarshad2003@gmail.com>
2026-02-05 19:24:38 +0500

    feat: added argocd-install script to install argocd via helm with argocd-values.yaml values

---
+++ argocd-install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+NAMESPACE="argocd"
+SECRET_NAME="argocd-initial-admin-secret"
+
+helm repo add argo https://argoproj.github.io/argo-helm
+helm repo update
+
+helm upgrade --install argocd argo/argo-cd \
+  -n $NAMESPACE \
+  --create-namespace \
+  -f argocd/argocd-values.yaml
+
+until kubectl get secret $SECRET_NAME -n $NAMESPACE > /dev/null 2>&1; do
+  echo "waiting for secret to generate..."
+  sleep 2
+done
+
+echo "---------------------------------------------------"
+echo "---------------------------------------------------"
+
+echo "Secret found. Extracting password..."
+
+ADMIN_PASSWORD=$(kubectl get secret $SECRET_NAME -n $NAMESPACE -o jsonpath="{.data.password}" | base64 -d)
+
+echo "---------------------------------------------------"
+echo "ArgoCD Admin Password: $ADMIN_PASSWORD"
+echo "---------------------------------------------------"
